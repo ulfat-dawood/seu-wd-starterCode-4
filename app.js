a6d1d91b593aeafe61cfc4cf1d355a91e07e2f2a
Ulfat Dawood <ulfat_dawood@outlook.com>
2021-12-16 00:37:39 +0300

    starter code files upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//Form
+var button= document.querySelector(''); 
+var input= document.querySelector(''); 
+//City & weather
+var city= document.querySelector(''); 
+var weather= document.querySelector('');
+//Temperature
+var currentTemp= document.querySelector('');
+var minTemp= document.querySelector('');
+var maxTemp= document.querySelector('');
+//Info
+var wind= document.querySelector('');
+var humidity= document.querySelector('');
+var pressure= document.querySelector('');
+var icon= document.querySelector('');
+
+//Open weather API key: 
+const apiKey= 'YOUR API key here'; 
+
+
+//Button Event listener 'click' 
